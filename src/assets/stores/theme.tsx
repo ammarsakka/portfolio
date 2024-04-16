7dd24c5b39d643cf3defc0ae3bd78bb785aed544
arsakka <ammar.sakka@agmostudio.com>
2024-04-16 12:07:24 +0800

    update theme logic

--- src/assets/stores/theme.tsx
+++ src/assets/stores/theme.tsx
@@ -2,16 +2,29 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 let storageName = "theme";
 
+const getCurrentTheme = () => {
+    const themeStorage = window.localStorage.getItem(storageName);
+    const themeDefault = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (themeStorage) {
+        return themeStorage;
+    }
+
+    if (themeDefault) {
+        return "dark";
+    }
+
+    return "light";
+};
+
 export interface ThemeState {
     value: string;
 }
 
 const initialState: ThemeState = {
-    value:
-        window.localStorage.getItem(storageName) ||
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "dark"
-            : "light",
+    value: getCurrentTheme(),
 };
 
 export const themeSlice = createSlice({
