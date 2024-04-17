449d8ae2fe1d20c18da90d21dd79d0b9d3ac6bb9
arsakka <ammar.sakka@agmostudio.com>
2024-04-17 09:15:06 +0800

    update hero text

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,16 +1,25 @@
 function Hero() {
     return (
         <div className="grid grid-cols-2 justify-between gap-4 items-center">
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-6">
                 <h1 className="font-semibold text-4xl">
                     Hello this is{" "}
                     <span className="text-orange-500 uppercase font-bold">
                         Developer
                     </span>
                 </h1>
-                <div className="flex flex-col gap-6">
-                    <p className="text-lg">I'm an software engineer</p>
-                    <p className="text-base">Welcome to my website!</p>
+                <div className="flex flex-col gap-2">
+                    <p className="text-lg">Welcome to my portfolio!</p>
+                    <p className="text-sm">
+                        I graduated as a Software Engineer from University Kuala
+                        Lumpur, Malaysia, where I honed my technical skills and
+                        passion for coding. Additionally, I hold another degree
+                        in Accounting from my home country. Balancing the worlds
+                        of technology and finance has given me a unique
+                        perspective, blending analytical thinking with creative
+                        problem-solving. I'm excited to bring this diverse
+                        background to everything I do!
+                    </p>
                 </div>
             </div>
             <div className="flex justify-end">
