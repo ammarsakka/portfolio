00dfcdce00ae920eca49ee9dada780d905b7fe65
Ammar Sakka <117344965+ammarsakka@users.noreply.github.com>
2024-04-19 20:58:12 +0800

    main

--- src/components/contact/constants/contact.tsx
+++ src/components/contact/constants/contact.tsx
@@ -1,2 +1,1 @@
-// export const EMAIL_SERVER_URL = "https://www.friendlymarketing.us/api";
-export const EMAIL_SERVER_URL = "http://localhost:3000/api";
+export const EMAIL_SERVER_URL = "https://www.friendlymarketing.us/api";
