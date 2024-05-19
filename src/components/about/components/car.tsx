fda94b931be5b7f91bcb4b670a666359e034fd8f
Ammar Sakka <117344965+ammarsakka@users.noreply.github.com>
2024-05-19 12:54:31 +0800

    update the 3D model UI

--- src/components/about/components/car.tsx
+++ src/components/about/components/car.tsx
@@ -1,66 +1,29 @@
-import { useFrame } from "@react-three/fiber";
-import React, { useEffect, useRef, useState } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
-import { useGLTF } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 function Car(props: any) {
-  const group = useRef();
-  const [isMobile, setMobile] = useState(false);
-  const { nodes, animations, cameras } = useGLTF(
-    `${process.env.PUBLIC_URL}/models/hover_bike_-_the_rocket.glb`
+  const ref = useRef<THREE.Mesh>(null!);
+
+  // Load 3D model
+  const { scene, animations } = useLoader(
+    GLTFLoader,
+    `${process.env.PUBLIC_URL}/models/stylized_plane.glb`
   );
-  let mixer = new THREE.AnimationMixer(nodes._rootJoint);
 
-  animations.forEach((clip) => {
-    const action = mixer.clipAction(clip);
-    action.play();
-  });
+  // Define mixer for animations
+  let mixer = new THREE.AnimationMixer(scene);
 
-  useFrame((state, delta) => {
-    mixer.update(delta);
-  });
+  animations.forEach((clip) => mixer.clipAction(clip).play());
 
-  const handleWindowSize = () => {
-    const size = window.innerWidth;
-
-    if (size <= 900) {
-      setMobile(true);
-      return;
-    }
-
-    setMobile(false);
-  };
-
-  useEffect(handleWindowSize, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowSize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowSize);
-    };
-  }, []);
+  useFrame((state, delta) => mixer.update(delta));
 
   return (
-    <group ref={group} {...props} dispose={null}>
-      <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 125.125]}>
-        <group
-          name="Object_7"
-          position={[Math.PI / 2, 0, 0]}
-          scale={isMobile ? 1.2 : 1}
-        >
-          <primitive
-            object={nodes._rootJoint}
-            position={isMobile ? [-1.5, 3, -0.5] : [0, 1.5, -1]}
-            rotation={[0.1, 0.2, 6.0]}
-          />
-          <primitive object={cameras} makeDefault />
-        </group>
-      </group>
-    </group>
+    <mesh ref={ref}>
+      <primitive object={scene} scale={6} rotation={[0.2, -1, 0]} />
+    </mesh>
   );
 }
 
 export default Car;
-
-useGLTF.preload(`${process.env.PUBLIC_URL}/models/hover_bike_-_the_rocket.glb`);
--- src/components/about/components/carModel.tsx
+++ src/components/about/components/carModel.tsx
@@ -1,26 +1,20 @@
+import { CameraControls, Center, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
 import Car from "./car";
 
 function CarModel() {
-    return (
-        <Canvas className="!h-[400px] mobile:!h-[300px]">
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight
-                position={[10, 10, 10]}
-                angle={0.25}
-                penumbra={1}
-                decay={0}
-                intensity={Math.PI}
-            />
-            <pointLight
-                position={[-10, -10, -10]}
-                decay={0}
-                intensity={Math.PI}
-            />
-            <Car />
-        </Canvas>
-    );
+  return (
+    <Canvas className="!h-[300px] mobile:!h-[400px]">
+      <PerspectiveCamera makeDefault fov={4} position={[0, 0, 80]} />
+      <ambientLight position={[1, 1, 1]} intensity={1.5} />
+      <spotLight intensity={1} decay={0} position={[10, 10, 10]} />
+      <pointLight intensity={1} decay={0} />
+      <CameraControls />
+      <Center>
+        <Car />
+      </Center>
+    </Canvas>
+  );
 }
 
 export default CarModel;
